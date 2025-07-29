220d1305c4e20bf351974e30b27701a7cff09bc0
chrissy <chrissyrodgers20@gmail.com>
2025-07-29 17:52:53 -0500

    changed react-router in layout.jsx

--- check-your-bags/src/Layout.jsx
+++ check-your-bags/src/Layout.jsx
@@ -1,13 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router";
 import NavBar from "./Components/Navbar";
 
 
 export default function Layout() { 
     return (
         <>
-            <NavBar/> 
+            <NavBar /> 
             <main>
-                <Outlet/>
+                <Outlet />
             </main>
         
         </>
